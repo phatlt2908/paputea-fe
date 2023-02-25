e0b79a0775b1e27044b5abaa0adac345e7443a59
phatlt <phatlt@jienielab.asia>
2023-02-25 16:18:50 +0700

    add about page

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,9 +1,7 @@
 import classes from "./logo.module.css";
 
 function Logo() {
-  return (
-    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-  );
+  return <img src="/logo.png" width="28" height="28" />;
 }
 
 export default Logo;
--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -14,7 +14,7 @@ function MainNavigation() {
   return (
     <header>
       <nav
-        className="navbar bg-base-100"
+        className="navbar bg-base-100 shadow-sm"
         role="navigation"
         aria-label="main navigation"
       >
@@ -45,21 +45,33 @@ function MainNavigation() {
             <a className="navbar-item is-active">Trang chủ</a>
 
             <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">Phụ Huynh Học Sinh</a>
+              <a className="navbar-link">Phụ Huynh</a>
               <div className="navbar-dropdown">
-                <a className="navbar-item">Học phí tham khảo</a>
-                <a className="navbar-item">Các vấn đề lưu ý</a>
+                <a className="navbar-item">Đăng kí tìm gia sư, giáo viên</a>
+                <a className="navbar-item">Đăng kí học tại Trung tâm Paputea</a>
                 <hr className="navbar-divider" />
-                <a className="navbar-item">Tìm gia sư</a>
+                <a className="navbar-item">Học phí tham khảo</a>
+                <a className="navbar-item">Phụ huynh cần biết</a>
+              </div>
+            </div>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">Học sinh</a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item">Đăng kí tìm lớp</a>
+                <a className="navbar-item">Đăng kí tìm gia sư</a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item">Học phí tham khảo</a>
+                <a className="navbar-item">Học sinh cần biết</a>
               </div>
             </div>
             <div className="navbar-item has-dropdown is-hoverable">
               <a className="navbar-link">Gia sư</a>
               <div className="navbar-dropdown">
-                <a className="navbar-item">Danh sách các lớp</a>
-                <a className="navbar-item">Điều khoản</a>
+                <a className="navbar-item">Đăng kí gia sư</a>
+                <a className="navbar-item">Lớp mới chưa giao</a>
                 <hr className="navbar-divider" />
-                <a className="navbar-item">Đăng ký làm gia sư</a>
+                <a className="navbar-item">Quy định khi nhận lớp</a>
+                <a className="navbar-item">Gia sư cần biết</a>
               </div>
             </div>
             <a className="navbar-item">Tài liệu</a>
