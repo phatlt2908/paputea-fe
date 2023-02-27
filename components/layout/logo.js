ed91e3cc80d799a45ffdec2df9e2cf07b87cc974
phatlt <phatlt@jienielab.asia>
2023-02-27 20:14:23 +0700

    update logo image

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from "./logo.module.css";
 
 function Logo() {
-  return <img src="/logo.png" width="28" height="28" />;
+  return <Image src="/logo.png" alt="Logo" width={28} height={28} />;
 }
 
 export default Logo;
