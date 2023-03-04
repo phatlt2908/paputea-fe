23d5d287fc04f972d47cac0a65e83953b56d9822
phatlt <phatlt@jienielab.asia>
2023-03-04 16:47:01 +0700

    complete footer

--- components/layout/main-footer.js
+++ components/layout/main-footer.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Logo from "./logo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./main-footer.module.scss";
 
@@ -9,18 +13,22 @@ function MainFooter() {
   return (
     <footer className={classes.main}>
       <div className={classes.newsletter}>
-        <section className="bd-index-section bd-newsletter-box">
-          <div className="bd-newsletter-heading columns is-desktop">
-            <div className="column">
-              <span className="icon has-text-primary is-size-2-widescreen">
-                <font-awesome-icon icon="paper-plane" />
+        <section className={`${classes.section} ${classes.box}`}>
+          <div className={`${classes.heading} columns is-desktop`}>
+            <div className={`${classes.column} column`}>
+              <span
+                className={`${classes.icon} icon has-text-primary is-size-2-widescreen`}
+              >
+                <FontAwesomeIcon icon={faPaperPlane} />
               </span>
 
-              <h2 className="title has-text-black mb-0 is-size-2-widescreen">
+              <h2
+                className={`${classes.title} title has-text-black mb-0 is-size-2-widescreen`}
+              >
                 <strong>Phản hồi</strong>
                 <small className="mt-2">
                   Rất mong nhận được những thắc mắc hoặc đóng góp ý kiến từ các
-                  bạn, để ĐâyNè.run ngày càng hoàn thiện hơn!
+                  bạn, để <strong>Paputea</strong> ngày càng hoàn thiện hơn! 😍
                 </small>
               </h2>
             </div>
@@ -31,11 +39,11 @@ function MainFooter() {
                   <input
                     type="email"
                     name="email"
-                    className="input is-medium is-primary"
+                    className="input is-medium"
                     placeholder="email"
                   />
                   <span className="icon is-small is-left">
-                    <font-awesome-icon icon="envelope" />
+                    <FontAwesomeIcon icon={faEnvelope} />
                   </span>
                 </div>
                 <p className="help">Không bắt buộc</p>
@@ -44,7 +52,7 @@ function MainFooter() {
               <div className="field">
                 <div className="control">
                   <textarea
-                    className="textarea is-medium is-primary"
+                    className="textarea is-medium"
                     placeholder="Nội dung phản hồi..."
                   ></textarea>
                 </div>
@@ -53,7 +61,7 @@ function MainFooter() {
               <div className="control">
                 <button className="button is-medium is-primary">
                   <span className="icon is-medium">
-                    <font-awesome-icon icon="comment" />
+                    <FontAwesomeIcon icon={faComment} />
                   </span>
                   <span>
                     <strong>Gửi phản hồi</strong>
@@ -64,47 +72,93 @@ function MainFooter() {
           </div>
         </section>
       </div>
-      <div className="bd-footer">
+      <div className={classes.footer}>
         <div className="container">
-          <div className="bd-footer-links">
+          <div className={classes.links}>
             <div className="columns is-desktop">
               <div className="column">
-                <h4 className="bd-footer-title">
-                  <strong>DayNe.run</strong> bởi
-                  <a target="_blank" href="https://www.facebook.com/phatamao">
-                    PhátLT
-                  </a>
+                <h4 className={classes.footerTitle}>
+                  <strong>Paputea</strong>
                 </h4>
-                <p className="bd-footer-tsp">
-                  Vì mục đích chia sẻ
-                  <br />
-                  và phi lợi nhuận
+                <p className={classes.tsp}>
+                  Lorem Ipsum chỉ đơn giản là một
+                  <br /> đoạn văn bản giả, được dùng <br /> vào việc trình bày
+                  và dàn trang
+                  <br /> phục vụ cho in ấn
                 </p>
               </div>
               <div className="column">
-                <p className="bd-footer-link-title">
+                <p className={classes.linkTitle}>
                   <Link href="/">
                     <span>Danh mục</span>
                   </Link>
                 </p>
-                <p className="bd-footer-link">
+                <p className={classes.link}>
                   <Link href="/">
-                    <span>Games</span>
+                    <span>Dành cho phụ huynh</span>
                   </Link>
                 </p>
-                <div className="column">
-                  <h4 className="bd-footer-title">
-                    <strong>Hỗ trợ</strong> DayNe.run 😃
-                  </h4>
-                  <p className="bd-footer-link bd-has-subtitle">
-                    <a
-                      target="_blank"
-                      href="https://me.momo.vn/vjIyuZsGU1UAFlfkIRsn"
-                    >
-                      <strong> Momo </strong>
-                      <em> donate qua Momo </em>
-                    </a>
-                  </p>
+                <p className={classes.link}>
+                  <Link href="/">
+                    <span>Dành cho học sinh</span>
+                  </Link>
+                </p>
+                <p className={classes.link}>
+                  <Link href="/">
+                    <span>Dành cho gia sư</span>
+                  </Link>
+                </p>
+                <p className={classes.link}>
+                  <Link href="/">
+                    <span>Tin tức</span>
+                  </Link>
+                </p>
+              </div>
+              <div className="column">
+                <h4 className={classes.footerTitle}>
+                  <strong>Liên hệ</strong> 👀
+                </h4>
+                <p className={`${classes.link} ${classes.subtitle}`}>
+                  <a
+                    className="mb-1"
+                    target="_blank"
+                    href="https://www.google.com/maps?ll=10.876283,106.654802&z=15&t=m&hl=en&gl=US&mapclient=embed&q=%C4%90.+L%C3%AA+Th%E1%BB%8B+Ri%C3%AAng+Th%E1%BB%9Bi+An+Qu%E1%BA%ADn+12+Th%C3%A0nh+ph%E1%BB%91+H%E1%BB%93+Ch%C3%AD+Minh"
+                  >
+                    <span>
+                      <FontAwesomeIcon icon={faLocationDot} />
+                      <span className="ml-2">
+                        C21 Lê Thị Riêng, P. Thới An, Q. 12, TP. Hồ Chí Minh
+                      </span>
+                    </span>
+                    <em>Địa chỉ</em>
+                  </a>
+                  <a className="mb-1" target="_blank" href="tel:0941388990">
+                    <span>
+                      <FontAwesomeIcon icon={faPhone} />
+                      <span className="ml-2">0941 388 990</span>
+                    </span>
+                    <em>Phone / Zalo</em>
+                  </a>
+                  <a
+                    className="mb-1"
+                    target="_blank"
+                    href="mailto:trungtampaputea@gmail.com"
+                  >
+                    <span>
+                      <FontAwesomeIcon icon={faEnvelope} />
+                      <span className="ml-2">trungtampaputea@gmail.com</span>
+                    </span>
+                    <em>Email</em>
+                  </a>
+                </p>
+                <div className="box mt-2">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9319.481799957115!2d106.64854740487561!3d10.860243755632828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529ddf5219545%3A0x8582b7850ec0e767!2zxJAuIEzDqiBUaOG7iyBSacOqbmcsIFRo4bubaSBBbiwgUXXhuq1uIDEyLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1677922492441!5m2!1sen!2s"
+                    width="100%"
+                    height="100%"
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
                 </div>
               </div>
             </div>
