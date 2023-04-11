4ec13ee4a126ad5c7d4330630e7461d952369aae
phatlt <phatlt@jienielab.asia>
2023-04-11 21:32:57 +0700

    update ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,12 +12,10 @@ module.exports = {
 
       env: {
         NODE_ENV: "development",
-        PORT: 3100,
         ...require("dotenv").config({ path: ".env.development" }).parsed,
       },
       env_production: {
         NODE_ENV: "production",
-        PORT: 3101,
         ...require("dotenv").config({ path: ".env.production" }).parsed,
       },
     },
