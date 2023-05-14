673fe2f13f192cb7f311931009fcd9457738237e
phatlt <phatlt@jienielab.asia>
2023-05-14 20:28:09 +0700

    update port and config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  server: {
+    port: process.env.PORT || 3000,
+  },
 };
 
 module.exports = nextConfig;
